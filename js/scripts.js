653e887a5973e927f9cace60f4dc3b3883934795
Patrick Leahy and Sowmya Dinavahi <patrick+sowmya@>
2017-03-23 15:40:29 -0700

    Add radio buttons for operator and add the function to operator to add,subtract,divide and multiply

--- js/scripts.js
+++ js/scripts.js
@@ -15,63 +15,15 @@ var divide = function(number1, number2) {
 
 // User interface (or front-end) logic:
 
-$(document).ready(function(){
-  $("form#add").submit(function(event){
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
 
-  var result = add(number1, number2);
 
-  $("#output").text(result);
-  event.preventDefault();
-  });
-
-  $("form#subtract").submit(function(event){
-  var number1 = parseInt($("#subtract1").val());
-  var number2 = parseInt($("#subtract2").val());
-
-  var result = subtract(number1, number2);
-
-  $("#output").text(result);
-  event.preventDefault();
-  });
-
-  $("form#multiply").submit(function(event){
-  var number1 = parseInt($("#multiply1").val());
-  var number2 = parseInt($("#multiply2").val());
-
-  var result = multiply(number1, number2);
-
-  $("#output").text(result);
-  event.preventDefault();
-  });
-
-  $("form#divide").submit(function(event){
-  var number1 = parseInt($("#divide1").val());
-  var number2 = parseInt($("#divide2").val());
-
-  var result = divide(number1, number2);
-
-  $("#output").text(result);
-  event.preventDefault();
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result = window[operator](number1, number2);
+    $("#output").text(result);
   });
 });
-
-
-// var number1 = parseInt(prompt("Enter a number:"));
-// var operator = prompt("Enter the operator: ('add', 'subtract', 'multiply', 'divide')").toLowerCase();
-// var number2 = parseInt(prompt("Enter another number:"));
-
-// if (operator == "add"){
-//   alert( add(number1 ,number2));
-// }else if (operator == "subtract"){
-//   alert(subtract(number1,number2));
-// }else if (operator =="multiply"){
-//   alert(multiply(number1,number2));
-// }else if (operator =="divide"){
-//   alert(divide(number1,number2));
-// }else {
-// alert("wrong input");
-// }
-// var result = divide(number1, number2);
-// alert(result);
